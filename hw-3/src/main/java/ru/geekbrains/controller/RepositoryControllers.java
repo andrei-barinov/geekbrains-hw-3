434afc334775686a5164d8f0cba613439cede34d
Andre <ncl8@mail.ru>
2021-03-11 12:03:02 +0300

    version 2.0

---
+++ hw-3/src/main/java/ru/geekbrains/controller/RepositoryControllers.java
@@ -0,0 +1,2 @@
+package ru.geekbrains.controller;public class RepositoryControllers {
+}
---
+++ hw-3/src/main/java/ru/geekbrains/dao/RepositoryDAO.java
@@ -0,0 +1,32 @@
+package ru.geekbrains.controller;
+
+import org.springframework.stereotype.Component;
+import ru.geekbrains.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class RepositoryDAO {
+    private static int PRODUCT_COUNT;
+    private List<Product> productList;
+
+    {
+        productList = new ArrayList<>();
+
+        productList.add(new Product(++PRODUCT_COUNT, "Orange", 10));
+        productList.add(new Product(++PRODUCT_COUNT, "Banana", 20));
+        productList.add(new Product(++PRODUCT_COUNT, "Kiwi", 30));
+        productList.add(new Product(++PRODUCT_COUNT, "Pineapple", 40));
+        productList.add(new Product(++PRODUCT_COUNT, "Apple", 50));
+
+    }
+
+    public List<Product> index(){
+        return productList;
+    }
+
+    public Product show(int id){
+        return productList.stream().filter(product -> product.getId() == id).findAny().orElse(null);
+    }
+}
